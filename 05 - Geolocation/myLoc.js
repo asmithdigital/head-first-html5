f948b026d239fa75adb3e9143653e040c953e975
Andrew Smith <andrew@asmithdigital.com>
2016-05-24 20:57:39 +0930

    first commit

---
+++ 05 - Geolocation/myLoc.js
@@ -0,0 +1,166 @@
+
+window.onload = getMyLocation;
+
+var watchId = null;
+var options = {enableHighAccuracy: true, timeout: 100, maximumAge: 0}
+
+function getMyLocation(){
+	if (navigator.geolocation) {
+		// console.log(navigator.geolocation.getCurrentPosition(displayLocation, displayError));
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+		var watchButton = document.getElementById('watch');
+		watchButton.onclick = watchLocation;
+		var clearWatchButton = document.getElementById('clearWatch');
+		clearWatchButton.onclick = clearWatch;
+	} else{
+		alert("Oops, no geolocation support");
+	};
+}
+
+function watchLocation(){
+	watchId = navigator.geolocation.watchPosition(
+						displayLocation, 
+						displayError,
+						options);
+}
+
+function scrollMapToPosition(coords) {
+	var latitude = coords.latitude;
+	var longitude = coords.longitude;
+
+	var latlong = new google.maps.LatLng(latitude, longitude);
+	map.panTo(latlong);
+
+	// add the new marker
+	addMarker(map, latlong, "Your new location", "You moved to: " + 
+								latitude + ", " + longitude);
+}
+
+function clearWatch(){
+	if(watchId){
+		navigator.geolocation.clearWatch(watchId);
+		watchId = null;
+	}
+}
+
+function displayLocation(position){
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	var div = document.getElementById("location");
+	div.innerHTML = "You are a latitude: " + latitude + " , longitude: " + longitude;
+	div.innerHTML += "with " + position.coords.accuracy + " meters accuracy";
+	div.innerHTML += " (found in " + options.timeout + " milliseconds)";
+
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById('distance');
+	distance.innerHTML = "You are " + km + " km away from the WickedlySmart HQ";
+
+	if (map == null) {
+		showMap(position.coords);
+	} else {
+		scrollMapToPosition(position.coords);
+	}
+}
+
+function displayError(error){
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied by user",
+		2: "Position is not available",
+		3: "Request timed out"
+	};
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2){
+		errorMessage = errorMessage + " " + error.message;
+	}
+
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
+
+	options.timeout += 100;
+	navigator.geolocation.getCurrentPosition(
+		displayLocation,
+		displayError,
+		options);
+	div.innerHTML += " ... checking again with timout=" + options.timeout;
+}
+
+
+// --------------------- Ready Bake ------------------
+//
+// Uses the Spherical Law of Cosines to find the distance
+// between two lat/long points
+//
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	var Radius = 6371; // radius of the Earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+					Math.cos(startLatRads) * Math.cos(destLatRads) *
+					Math.cos(startLongRads - destLongRads)) * Radius;
+
+	return distance;
+}
+
+function degreesToRadians(degrees) {
+	radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+// ------------------ End Ready Bake -----------------
+
+// WickedlySmart HQ coords
+var ourCoords = {
+	latitude: 47.624851,
+	longitude: -122.52099
+}
+
+
+// Google Maps API
+
+var map;
+
+function showMap(coords){
+		var googleLatAndLong = new google.maps.LatLng(coords.latitude, 
+												  coords.longitude);
+		var mapOptions = {
+			zoom: 10,
+			center: googleLatAndLong,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var mapDiv = document.getElementById("map");
+		map = new google.maps.Map(mapDiv, mapOptions);
+
+		// add the user marker
+		var title = "Your Location";
+		var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+		addMarker(map, googleLatAndLong, title, content);
+}
+
+
+function addMarker(map, latlong, title, content){
+	var markerOptions = {
+		position: latlong,
+		map: map,
+		title: title,
+		clickable: true
+	}
+	var marker = new google.maps.Marker(markerOptions);
+
+	var infoWindowOptions = {
+		content: content,
+		position: latlong
+	};
+
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infoWindow.open(map);
+	});
+}
+
+
